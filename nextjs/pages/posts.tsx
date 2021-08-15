55b6cf87cf31ddb47b356359bdf80839365f70cb
nemrosim <nemrosim1988@gmail.com>
2021-08-15 12:22:45 +0300

    feat: fetch `posts` in `Posts` page, add `Head` component

--- nextjs/pages/posts.tsx
+++ nextjs/pages/posts.tsx
@@ -1,5 +1,42 @@
-const Posts = () => {
-    return <h1>Posts page</h1>
+import React from "react";
+import Head from "next/head";
+import { GetServerSideProps } from "next";
+import { SERVER_HOST } from "../constants";
+import { PostDto } from "../types";
+import { PostCard } from "../components";
+
+interface PostsProps {
+    posts: Array<PostDto>;
+}
+
+const Posts: React.FC<PostsProps> = ({posts}) => {
+    return (
+        <>
+            <Head>
+                <title>NextJS. Posts page</title>
+                <meta name="description" content="Posts page description"/>
+            </Head>
+            <h1>Posts page</h1>
+            <div className="container">
+                <div className="row">
+                    {posts.map(post => {
+                        return <div
+                            key={post.id}
+                            className="col"
+                        >
+                            <PostCard post={post}/>
+                        </div>
+                    })}
+                </div>
+            </div>
+        </>);
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+    const res = await fetch(`${SERVER_HOST}/posts`);
+    const posts = await res.json();
+
+    return {props: {posts}}
 }
 
 export default Posts;
